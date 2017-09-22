2355fcc11937ba84de1b6146911db08e1c389914
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-22 14:59:24 -0400

    Storyboard Changed.

---
+++ ChatApp/Controller/ChatVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChatVC.swift
+//  ChatApp
+//
+//  Created by vamsi krishna reddy kamjula on 9/22/17.
+//  Copyright © 2017 applicationDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}

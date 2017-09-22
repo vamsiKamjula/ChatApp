2927c302c39e2a065358191673baf545ea20d4d6
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-22 16:32:46 -0400

    All Errors Removed.

--- ChatApp/Controller/ChatVC.swift
+++ ChatApp/Controller/ChatVC.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class ChatVC: UIViewController {
-
+    
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
     }
 }

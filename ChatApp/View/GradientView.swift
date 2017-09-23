761816f7082c288815b25a0b44338545a146e3d6
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-23 11:58:18 -0400

    Gradient View and Side Menu Updated.

---
+++ ChatApp/View/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  ChatApp
+//
+//  Created by vamsi krishna reddy kamjula on 9/23/17.
+//  Copyright © 2017 applicationDevelopment. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.black {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x:0, y:0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}

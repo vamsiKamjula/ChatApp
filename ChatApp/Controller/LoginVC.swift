38ae2163571e7a8fcdfb7f463c4fe9f8f200b735
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-23 12:57:50 -0400

    Login View updated.

---
+++ ChatApp/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  ChatApp
+//
+//  Created by vamsi krishna reddy kamjula on 9/23/17.
+//  Copyright © 2017 applicationDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ ChatApp/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatApp
+//
+//  Created by vamsi krishna reddy kamjula on 9/23/17.
+//  Copyright © 2017 applicationDevelopment. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"

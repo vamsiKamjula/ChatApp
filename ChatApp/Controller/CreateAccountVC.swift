87d1d5f27457b6395b8e7079c5f0d5c3d04e55d3
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-25 14:47:21 -0400

    Avatar Image Picker updated.

--- ChatApp/Controller/CreateAccountVC.swift
+++ ChatApp/Controller/CreateAccountVC.swift
@@ -23,8 +23,15 @@ class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
+    
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
--- ChatApp/Services/UserDataService.swift
+++ ChatApp/Services/UserDataService.swift
@@ -13,11 +13,11 @@ class UserDataService {
     static let instance = UserDataService()
     
     public private(set) var id = ""
-    public private(set) var avatarColor = ""
     public private(set) var avatarName = ""
+    public private(set) var avatarColor = ""
     public private(set) var email = ""
     public private(set) var name = ""
-    
+
     func setUserData(id: String, color: String, avatarName: String, email: String, name: String) {
         self.id = id
         self.avatarColor = color

40e2b321b26762f990f166d263a243e2f3588ebb
vamsi krishna reddy kamjula <vamsikvk916@gmail.com>
2017-09-27 13:40:21 -0400

    Channel List Updated.

--- ChatApp/Controller/ChannelVC.swift
+++ ChatApp/Controller/ChannelVC.swift
@@ -9,15 +9,18 @@
 import UIKit
 import FirebaseAuth
 
-class ChannelVC: UIViewController {
+class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var userImg: CircleImage!
+    @IBOutlet weak var tableView: UITableView!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {}
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
@@ -52,6 +55,24 @@ class ChannelVC: UIViewController {
             present(profile, animated: true, completion: nil)
         } else{
             performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return MessageService.instance.channels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "channelCell", for: indexPath) as? ChannelCell {
+            let channel = MessageService.instance.channels[indexPath.row]
+            cell.configureCell(channel: channel)
+            return cell
+        } else {
+            return UITableViewCell()
         }
     }
 }
---
+++ ChatApp/View/ChannelCell.swift
@@ -0,0 +1,35 @@
+//
+//  ChannelCell.swift
+//  ChatApp
+//
+//  Created by vamsi krishna reddy kamjula on 9/27/17.
+//  Copyright © 2017 applicationDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var channelName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+}
